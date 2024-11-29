1291b56f6867c0ea5ba4bd79ecce69a9588999d7
Prathik Sridhar <prathiksridhar23@gmail.com>
2024-11-29 11:16:32 +0530

    Pagination added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,11 +4,12 @@ class UsersController < ApplicationController
   before_action :require_same_user, only: [:edit, :update, :destroy]
 
   def show
-    @articles = @user.articles
+    @user = User.find(params[:id])
+    @articles = @user.articles.paginate(page: params[:page], per_page: 5)
   end
 
   def index
-    @users = User.all
+    @users = User.paginate(page: params[:page], per_page: 5)
   end
 
   def new
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_08_29_061931) do
+ActiveRecord::Schema[7.2].define(version: 2024_09_01_160232) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
@@ -25,5 +25,6 @@ ActiveRecord::Schema[7.2].define(version: 2024_08_29_061931) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.boolean "admin", default: false
   end
 end
